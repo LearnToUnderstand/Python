b372b357cb5871ba5c9db1b41a7dc88ac3fe060f
LearnToUnderstand <29694600+LearnToUnderstand@users.noreply.github.com>
2022-10-31 20:08:57 -0400

    added via upload, need to brush up on my git.
    start of my study.py program, to help me learn Network+ and Python3 at the same time.

---
+++ study.py
@@ -0,0 +1,27 @@
+# a python program to help me study
+# while also playing with Python at the same time
+
+# i can have a fill in the blank, remember to strip 
+# away the caps / add caps where needed, no matter how the input
+# is typed 
+
+print('NETWORK+ practice questions')
+input('Press any button to continue')
+
+i = 1
+while i > 0:
+    ans2 = input(\
+            'How fast of speed would you expect from cable internet? (in Mb) ')
+    if ans2 == '15':
+        print('YEP')
+        break
+    # can also use i = 0
+    else:
+        print('NOPE')
+
+#  THIS WORKS!!!  
+# now just need to segway into the next question
+
+
+
+
